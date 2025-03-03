7598f290fc4bf367194023f490f826a0a36675b4
joosepalasoo <joosep.alasoo@hkhk.edu.ee>
2025-03-03 12:15:56 +0200

    tehtud

--- php_yl_06.php
+++ php_yl_06.php
@@ -7,22 +7,24 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-        <div class="container">
+      <div class="container">
         <h1>genereeri 1-100</h1>
         <!--h06  -->
         <!--joosep alasoo it-23-->
         <!--03.03.25-->
         <?php
-        for ($i = 1; $i <= 100; $i++) {
-            if ($i % 10 == 0) {
-                echo '<div class="row">';
-            }
-            echo
+       for ($i = 1; $i <= 100; $i++) {
+        if ($i % 10 == 1) {
+            echo '<div class="row">';
         }
+        echo '<div class="col-sm-1">' . $i . '.</div>';
+        if ($i % 10 == 0) {
+            echo '</div>';
+        }
+    }
 
-        ?>
-            
-        </div>
+        ?>     
+      </div>
         
 
 
